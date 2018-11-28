71c25c3fec83f072364c15a923a7a2c1f2e2e22c
arnold <cheng2960483@163.com>
2018-11-28 22:19:15 +0800

    将项目改为spring boot工程

---
+++ src/main/java/us/zoom/spring/SpringBootConfig.java
@@ -0,0 +1,11 @@
+package us.zoom.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfig.class);
+    }
+}
---
+++ src/main/java/us/zoom/spring/SpringMvcConfig.java
@@ -0,0 +1,8 @@
+package us.zoom.spring;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringMvcConfig {
+
+}
