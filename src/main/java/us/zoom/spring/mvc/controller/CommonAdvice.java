117d5c886a46feebed0e9f215e04b1abc8d2af58
arnold <cheng2960483@163.com>
2018-12-01 23:12:11 +0800

    添加异常解析处理的测试代码。

---
+++ src/main/java/us/zoom/spring/mvc/controller/CommonAdvice.java
@@ -0,0 +1,33 @@
+package us.zoom.spring.mvc.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.HandlerMethod;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class CommonAdvice {
+    /**
+     * 异常解析捕获，可以捕获本类中抛出的所有指定类型的异常。
+     * ResponseStatus注解放在捕获异常的方法上，可以自定义异常在返回时的状态码
+     * @param exception
+     * @return
+     */
+    @ExceptionHandler(value = NullPointerException.class)
+    @ResponseBody
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    public Object handlerException(NullPointerException exception,HandlerMethod method){
+        
+        String name = method.getBeanType().getName();
+        Map map=new HashMap();
+        map.put("error",exception.getMessage());
+        map.put("method bean",name);
+        map.put("method name",method.getMethod().getName());
+        return map;
+    }
+}
