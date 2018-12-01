f630ab7f93e4cfb4c8db855581b46061e5deb286
wuhan <873383103@qq.com>
2018-12-02 00:12:52 +0800

    新增类测试审批权

---
+++ src/main/java/us/zoom/spring/domian/Person.java
@@ -0,0 +1,10 @@
+package us.zoom.spring.domian;
+
+public class Person {
+    String name ;
+
+    public static void main(String[] args) {
+        int i = Integer.parseInt("2");
+        System.out.println(i);
+    }
+}
