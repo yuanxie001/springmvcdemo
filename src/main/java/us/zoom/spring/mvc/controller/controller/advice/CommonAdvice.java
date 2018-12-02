de5148a9c4e798cc39edbcb9dd5b72f2537d03e0
arnold <cheng2960483@163.com>
2018-12-02 13:57:39 +0800

    添加model入参的逻辑。

--- src/main/java/us/zoom/spring/mvc/controller/controller/advice/CommonAdvice.java
+++ src/main/java/us/zoom/spring/mvc/controller/controller/advice/CommonAdvice.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.method.HandlerMethod;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,18 +15,20 @@ public class CommonAdvice {
     /**
      * 异常解析捕获，可以捕获本类中抛出的所有指定类型的异常。
      * ResponseStatus注解放在捕获异常的方法上，可以自定义异常在返回时的状态码
+     *
      * @param exception
      * @return
      */
     @ExceptionHandler(value = NullPointerException.class)
     @ResponseBody
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-    public Object handlerException(NullPointerException exception, HandlerMethod method){
-        String name = method.getBeanType().getName();
-        Map map=new HashMap();
-        map.put("error",exception.getMessage());
-        map.put("method_bean",name);
-        map.put("method_name",method.getMethod().getName());
+    public Object handlerException(NullPointerException exception, HandlerMethod method) {
+
+        String name1 = method.getBeanType().getName();
+        Map map = new HashMap();
+        map.put("error", exception.getMessage());
+        map.put("method_bean", name1);
+        map.put("method_name", method.getMethod().getName());
         return map;
     }
 }
