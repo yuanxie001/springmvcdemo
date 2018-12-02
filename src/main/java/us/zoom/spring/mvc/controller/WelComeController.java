470d625e32d511e91ba4b70ec1346b367a6a0a70
arnold <cheng2960483@163.com>
2018-12-02 20:56:22 +0800

    删除匹配所有的逻辑，不然静态资源无法处理到

--- src/main/java/us/zoom/spring/mvc/controller/WelComeController.java
+++ src/main/java/us/zoom/spring/mvc/controller/WelComeController.java
@@ -19,28 +19,33 @@ import javax.servlet.http.HttpServletResponse;
  * 默认测试，主要用来测试获取初始化默认配置的地方在哪里
  */
 @Controller
-public class WelComeController implements EnvironmentAware{
+public class WelComeController implements EnvironmentAware {
     private Environment environment;
-    @RequestMapping(value="/**")
-    @ResponseBody
-    public Object index(HttpServletRequest request){
-        return "like you!";
-    }
+    //移除匹配所有的方法，因为这个方法在的时候，静态资源永远都无法处理到。
+    // 导致这个原因主要在于request url解析器先解析的是requestMappingHandler这个。这个匹配到了，则无法匹配静态资源匹配器
+    // 顺序有关
+//    @RequestMapping(value="/**")
+//    @ResponseBody
+//    public Object index(HttpServletRequest request){
+//        return "like you!";
+//    }
 
-    @RequestMapping(value="/welcome",method = RequestMethod.GET)
+    @RequestMapping(value = "/welcome", method = RequestMethod.GET)
     @ResponseBody
-    public Object index2(HttpServletRequest request){
+    public Object index2(HttpServletRequest request) {
         String name = environment.getProperty("name");
         return "welcome you!";
     }
-    @RequestMapping(value="/id/{id:^1[34578]\\d{9}$}",method = RequestMethod.GET)
+
+    @RequestMapping(value = "/id/{id:^1[34578]\\d{9}$}", method = RequestMethod.GET)
     @ResponseBody
-    public Object path(@PathVariable("id")String id){
+    public Object path(@PathVariable("id") String id) {
         environment.getProperty("name");
-        return "welcome :"+id;
+        return "welcome :" + id;
     }
+
     @Override
     public void setEnvironment(Environment environment) {
-        this.environment=environment;
+        this.environment = environment;
     }
 }
