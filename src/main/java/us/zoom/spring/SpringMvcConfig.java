40d110110b7dca52c0e769e2b892bb4bc1f96844
arnold <cheng2960483@163.com>
2018-12-02 20:49:44 +0800

    添加静态资源的处理逻辑

--- src/main/java/us/zoom/spring/SpringMvcConfig.java
+++ src/main/java/us/zoom/spring/SpringMvcConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.resource.DefaultServletHttpRequestHandler;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 import org.springframework.web.servlet.view.xml.MappingJackson2XmlView;
@@ -87,5 +88,11 @@ public class SpringMvcConfig implements WebMvcConfigurer {
         views.add(mappingJackson2XmlView);
         contentNegotiatingViewResolver.setDefaultViews(views);
         return contentNegotiatingViewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/static/**");
     }
 }
