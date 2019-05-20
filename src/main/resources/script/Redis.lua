61afb269b93218f32ceeb2e5e5b609b9809cd9dd
arnold <arnold.zhu@zoom.us>
2019-05-20 13:00:55 +0800

    添加一个lua脚本

---
+++ src/main/resources/script/Redis.lua
@@ -0,0 +1,10 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: yingchun.zhu
+-- Date: 2019/5/20
+-- Time: 12:44
+-- To change this template use File | Settings | File Templates.
+--
+
+redis.call("ping")
+
