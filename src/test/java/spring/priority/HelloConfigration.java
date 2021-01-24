35ddf9ea42623844fa1bf24c3233341cc177a5a0
arnold <cheng2960483@163.com>
2021-01-24 21:17:10 +0800

    添加测试用例，用于debug 占位符Placeholder的处理逻辑

--- src/test/java/spring/priority/HelloConfigration.java
+++ src/test/java/spring/priority/HelloConfigration.java
@@ -2,8 +2,10 @@ package spring.priority;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages="spring.priority")
+@PropertySource(value = {"classpath:test.properties"})
 public class HelloConfigration {
 }
--- src/test/java/spring/priority/HelloController.java
+++ src/test/java/spring/priority/HelloController.java
@@ -1,12 +1,15 @@
 package spring.priority;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HelloController {
     @Autowired
     private HelloService helloService;
+    @Value("name")
+    private String name;
 
     public String say(){
         return "result:" + helloService.sayHello();
