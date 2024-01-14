ede4930a8e598c8612c8973edcc7857f67882a62
arnold <cheng2060483@163.com>
2024-01-14 19:45:05 +0800

    更新jar版本

--- src/main/java/store/xiaolan/spring/CacheConfig.java
+++ src/main/java/store/xiaolan/spring/CacheConfig.java
@@ -1,6 +1,9 @@
 package store.xiaolan.spring;
 
-import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.net.UnknownHostException;
@@ -38,14 +42,14 @@ public class CacheConfig {
         StringRedisTemplate template = new StringRedisTemplate();
         template.setConnectionFactory(redisConnectionFactory);
         // 使用Jackson2JsonRedisSerialize 替换默认序列化
-//        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-//
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-//        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+//        objectMapper.activateDefaultTyping(PolymorphicTypeValidator.Validity.ALLOWED);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
 //
 //        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
-        template.setValueSerializer(new GenericFastJsonRedisSerializer());
+//        template.setValueSerializer(new GenericFastJsonRedisSerializer());
 
         return template;
     }
--- src/main/java/store/xiaolan/spring/domian/User.java
+++ src/main/java/store/xiaolan/spring/domian/User.java
@@ -1,13 +1,7 @@
 package store.xiaolan.spring.domian;
 
 
-import jakarta.xml.bind.annotation.XmlAccessType;
-import jakarta.xml.bind.annotation.XmlAccessorType;
-import jakarta.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(name = "result")
-//@XStreamAlias("result")
-@XmlAccessorType(XmlAccessType.NONE)
 public class User {
     private String name;
     private String password;
