329eb4f4d20b8b47f90412f241f96ab132607ad7
cheng <cheng2960483@163.com>
2024-04-08 20:47:32 +0800

    简单处理了下pom启动不了的问题

--- src/main/java/store/xiaolan/spring/listener/TotalUrlListener.java
+++ src/main/java/store/xiaolan/spring/listener/TotalUrlListener.java
@@ -2,11 +2,15 @@ package store.xiaolan.spring.listener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.support.ServletRequestHandledEvent;
 
 import java.lang.invoke.MethodHandles;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 
 /**
  * 一个事件监听器,监听request的请求处理. 可以做处理完之后的分析逻辑.比如处理url的监听逻辑,处理请求日志啥的.
@@ -15,12 +19,21 @@ import java.lang.invoke.MethodHandles;
 @Component
 public class TotalUrlListener {
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+    private static final String CACHE_KEY_PREFIX = "count_{}";
+
+    // 创建一个DateTimeFormatter对象，指定格式
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     @EventListener(ServletRequestHandledEvent.class)
     public void onFlush(ServletRequestHandledEvent event){
         String requestUrl = event.getRequestUrl();
         String method = event.getMethod();
         String clientAddress = event.getClientAddress();
+        String format = formatter.format(Instant.now());
+        String cacheKey = String.format(CACHE_KEY_PREFIX, format);
         logger.info("请求方式为:{}.uri为:{},远程地址为:{}",method,requestUrl,clientAddress);
+        redisTemplate.opsForHash().increment(cacheKey,requestUrl,1);
     }
 }
