82eec8a16c462d2b468a5172ce942773420f3dfe
arnold <arnoldzhu056@gmail.com>
2024-04-30 18:16:54 +0800

    处理打包.发现打包不work.

--- src/main/java/store/xiaolan/spring/mvc/controller/CategoryController.java
+++ src/main/java/store/xiaolan/spring/mvc/controller/CategoryController.java
@@ -2,6 +2,7 @@ package store.xiaolan.spring.mvc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import store.xiaolan.spring.mvc.vo.CategoryVo;
 import store.xiaolan.spring.service.CategoryService;
@@ -14,7 +15,7 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
-    @GetMapping("/category")
+    @GetMapping("/product/category")
     public List<CategoryVo> list(){
         return categoryService.list();
     }
